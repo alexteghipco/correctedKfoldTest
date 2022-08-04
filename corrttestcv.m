0f0a2c7a8be07895c90a44552d32b2b76fd6d281
alexteghipco <alex.teghipco@uci.edu>
2022-08-04 12:28:26 -0700

    Add files via upload

---
+++ corrttestcv.m
@@ -0,0 +1,34 @@
+function [t,p] = corrttestcv(yhat1,yhat2,c)
+% [t,p] = corrttestML(yhat1,yhat2,c)
+%
+% Corrected repeated k-fold cross-validation test. c is the matlab
+% cvpartition object you used to generate predictions in yhat1 and yhat2
+% using two different models. Rows of yhat1 and yhat2 are the sample
+% predictions, and columns are the repeats. We convert t-values to p-values
+% using the statistics toolbox so make sure you have that installed.
+%
+% It is well documented that t-tests have a high type 1 error and low
+% replicability when comparing model performance across repeated k-fold cv.
+% Various proposals have tired to correct t-tests for type 1 errors. The
+% corrected repeated k-fold cv test implemented here does this while also
+% improving replicability.
+%
+% See: Bouckaert, R. R., & Frank, E. (2004, May). Evaluating the
+% replicability of significance tests for comparing learning algorithms. In
+% Pacific-Asia conference on knowledge discovery and data mining (pp.
+% 3-12). Springer, Berlin, Heidelberg.
+%
+% alex.teghipco@sc.edu
+
+k = c.NumTestSets;
+r = size(yhat1,2);
+
+for i = 1:k
+    for j = 1:r
+        d(i,j) = yhat1(test(c,i),j)-yhat2(test(c,i),j);
+    end
+end
+n = (1/(k*r))*sum(d(:)); % this amounts to the mean...
+s = (1/(k*(r-1)))*sum((d(:)-n).^2); % variance...
+t = sqrt((kr+(mean(c.TrainSize)/mean(c.TestSize)))*s);
+p = 1-tcdf(t,n-1);
